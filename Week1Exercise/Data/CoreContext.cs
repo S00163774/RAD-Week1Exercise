878c8ffa915a48ba35b0ef7bae277c79be5ac57c
S00163774 <S00163774@mail.itsligo.ie>
2018-01-22 15:59:02 +0000

    Up to controller errors

---
+++ Week1Exercise/Data/CoreContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using Week1Exercise.Models;
+
+namespace Week1Exercise.Data
+{
+    public class CoreContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public CoreContext()
+            : base("CoreContext")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
